b905fd93cfc396432212040452d3ce92c61077ec
Kadari Dave <dave@kadari>
2023-11-29 18:47:43 +0800

    lts before meeting

--- utils/PageFactory.py
+++ utils/PageFactory.py
@@ -99,6 +99,7 @@ def create_page_list(request=False,nav_list=False,operation="",page_name=False,a
         subdir=str(calframe[1][3])
         print("START perm_nav_list")
         perm_nav_list=extract_keys(nav_list, get_group_pages(user_group))
+        # perm_nav_list = _perm_nav_list if _perm_nav_list is not None else {}
         print("END perm_nav_list")
         
         if bool(nav_list.get(subdir,{})) == True:
